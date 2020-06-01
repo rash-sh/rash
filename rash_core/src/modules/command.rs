efe3f9e13569321a699fc5c3e0fc75753f33c5d1
Pando85 <pando855@gmail.com>
2020-06-01 07:30:30 +0200

    [module][command] Add argv param
    Allow parse command as list of args.
    
    Resolves: #60

--- rash_core/src/modules/command.rs
+++ rash_core/src/modules/command.rs
@@ -1,5 +1,5 @@
 use crate::error::{Error, ErrorKind, Result};
-use crate::modules::{get_param_bool, ModuleResult};
+use crate::modules::{get_param_bool, get_param_list, ModuleResult};
 use crate::vars::Vars;
 
 use std::process::Command;
@@ -9,7 +9,8 @@ use yaml_rust::Yaml;
 
 #[derive(Debug, PartialEq)]
 struct Params {
-    cmd: String,
+    cmd: Option<String>,
+    argv: Option<Vec<String>>,
     transfer_pid_1: bool,
 }
 
@@ -18,18 +19,24 @@ fn parse_params(yaml: Yaml) -> Result<Params> {
     let cmd = yaml
         .as_str()
         .or_else(|| yaml["cmd"].as_str())
-        .ok_or_else(|| {
-            Error::new(
-                ErrorKind::NotFound,
-                format!("Not cmd param found in: {:?}", yaml),
-            )
-        })?;
+        .map(String::from);
+
+    let argv = get_param_list(&yaml, "argv").ok();
     let transfer_pid_1 = get_param_bool(&yaml, "transfer_pid_1").or_else(|e| match e.kind() {
         ErrorKind::NotFound => Ok(false),
         _ => Err(e),
     })?;
+
+    if cmd.is_none() & argv.is_none() {
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            "argv or cmd must be defined",
+        ));
+    }
+
     Ok(Params {
-        cmd: cmd.to_string(),
+        cmd,
+        argv,
         transfer_pid_1,
     })
 }
@@ -39,7 +46,15 @@ pub fn exec(optional_params: Yaml, vars: Vars) -> Result<(ModuleResult, Vars)> {
     trace!("exec - params: {:?}", params);
 
     if params.transfer_pid_1 {
-        let mut args = params.cmd.split_whitespace();
+        let args_vec = match params.cmd {
+            Some(s) => s
+                .split_whitespace()
+                .map(String::from)
+                .collect::<Vec<String>>(),
+            // safe unwrap: verify in parse_params
+            None => params.argv.unwrap(),
+        };
+        let mut args = args_vec.iter();
 
         // safe unwrap: verify in parse_params
         let program = args.next().unwrap();
@@ -49,10 +64,21 @@ pub fn exec(optional_params: Yaml, vars: Vars) -> Result<(ModuleResult, Vars)> {
         return Err(Error::new(ErrorKind::SubprocessFail, error));
     }
 
-    let output = Command::new("/bin/sh")
-        .args(vec!["-c", &params.cmd])
-        .output()
-        .or_else(|e| Err(Error::new(ErrorKind::SubprocessFail, e)))?;
+    let output = if params.cmd.is_some() {
+        Command::new("/bin/sh")
+            .args(vec!["-c", &params.cmd.unwrap()])
+            .output()
+            .or_else(|e| Err(Error::new(ErrorKind::SubprocessFail, e)))?
+    } else {
+        let argv = params.argv.unwrap();
+        let mut args = argv.iter();
+        // safe unwrap: verify in parse_params
+        let program = args.next().unwrap();
+        Command::new(program)
+            .args(args)
+            .output()
+            .or_else(|e| Err(Error::new(ErrorKind::SubprocessFail, e)))?
+    };
 
     trace!("exec - output: {:?}", output);
     let stderr =
@@ -100,7 +126,8 @@ mod tests {
         assert_eq!(
             params,
             Params {
-                cmd: "ls".to_string(),
+                cmd: Some("ls".to_string()),
+                argv: None,
                 transfer_pid_1: false,
             }
         );
