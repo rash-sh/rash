4b6715f490c74ef16a4c57c5280d8131d1f3b943
Pando85 <pando855@gmail.com>
2020-06-10 17:42:39 +0200

    task: fix loop tera list render
    Signed-off-by: Pando85 <pando855@gmail.com>

--- rash_core/src/utils/tera.rs
+++ rash_core/src/utils/tera.rs
@@ -3,11 +3,17 @@ use crate::vars::Vars;
 
 use tera::Tera;
 
+#[inline(always)]
 pub fn render_string(s: &str, vars: Vars) -> Result<String> {
     let mut tera = Tera::default();
     trace!("rendering {:?}", &s);
     tera.render_str(s, &vars)
         .or_else(|e| Err(Error::new(ErrorKind::InvalidData, e)))
+}
+
+#[inline(always)]
+pub fn render_as_json(s: &str, vars: Vars) -> Result<String> {
+    render_string(&s.replace("}}", "| json_encode() | safe }}"), vars)
 }
 
 #[inline(always)]
