aebc193ffc7afa5e2ac1ea76ec21a173b6f8574e
pando85 <pando855@gmail.com>
2021-12-18 09:57:36 +0100

    fix(core): Remove Vars ownership in tera utils

--- rash_core/src/utils/tera.rs
+++ rash_core/src/utils/tera.rs
@@ -4,20 +4,20 @@ use crate::vars::Vars;
 use tera::Tera;
 
 #[inline(always)]
-pub fn render_string(s: &str, vars: Vars) -> Result<String> {
+pub fn render_string(s: &str, vars: &Vars) -> Result<String> {
     let mut tera = Tera::default();
     trace!("rendering {:?}", &s);
-    tera.render_str(s, &vars)
+    tera.render_str(s, vars)
         .map_err(|e| Error::new(ErrorKind::InvalidData, e))
 }
 
 #[inline(always)]
-pub fn render_as_json(s: &str, vars: Vars) -> Result<String> {
+pub fn render_as_json(s: &str, vars: &Vars) -> Result<String> {
     render_string(&s.replace("}}", "| json_encode() | safe }}"), vars)
 }
 
 #[inline(always)]
-pub fn is_render_string(s: &str, vars: Vars) -> Result<bool> {
+pub fn is_render_string(s: &str, vars: &Vars) -> Result<bool> {
     match render_string(
         &format!("{{% if {} | safe %}}true{{% else %}}false{{% endif %}}", s),
         vars,
