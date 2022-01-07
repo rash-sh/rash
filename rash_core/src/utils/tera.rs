95caa978c791cbc0b95012e27aa311843105d622
Alexander Gil <pando855@gmail.com>
2022-01-07 15:10:17 +0100

    feat(task): Add `omit()` for omitting parameters programmatically
    Resolves #70

--- rash_core/src/utils/tera.rs
+++ rash_core/src/utils/tera.rs
@@ -1,14 +1,50 @@
-use crate::error::{Error, ErrorKind, Result};
+use crate::error;
+use crate::error::Result;
 use crate::vars::Vars;
 
+use std::collections::HashMap;
+use std::error::Error as StdError;
+
+use serde_json::value::Value;
 use tera::Tera;
+
+fn omit(_: &HashMap<String, Value>) -> tera::Result<Value> {
+    Err(tera::Error::call_filter(
+        "omit",
+        tera::Error::msg("Not defined"),
+    ))
+}
+
+fn init_tera() -> Tera {
+    let mut tera = Tera::default();
+    tera.register_function("omit", omit);
+    tera
+}
+
+lazy_static! {
+    static ref TERA: Tera = init_tera();
+}
 
 #[inline(always)]
 pub fn render_string(s: &str, vars: &Vars) -> Result<String> {
-    let mut tera = Tera::default();
+    let mut tera = TERA.clone();
     trace!("rendering {:?}", &s);
-    tera.render_str(s, vars)
-        .map_err(|e| Error::new(ErrorKind::InvalidData, e))
+    tera.render_str(s, vars).map_err(|e| match e.source() {
+        Some(source_e) => match source_e.source() {
+            Some(source_source_e) => match source_source_e.source() {
+                Some(source_source_source_e) => {
+                    if source_source_source_e.to_string() == "Not defined" {
+                        error::Error::new(error::ErrorKind::OmitParam, "Param is omitted")
+                    } else {
+                        error::Error::new(error::ErrorKind::InvalidData, e)
+                    }
+                }
+                _ => error::Error::new(error::ErrorKind::InvalidData, e),
+            },
+            _ => error::Error::new(error::ErrorKind::InvalidData, e),
+        },
+        _ => error::Error::new(error::ErrorKind::InvalidData, e),
+    })
 }
 
 #[inline(always)]
@@ -40,5 +76,13 @@ mod tests {
         assert_eq!(r_true, true);
         let r_false = is_render_string("false", &Vars::new()).unwrap();
         assert_eq!(r_false, false);
+    }
+
+    #[test]
+    fn test_render_string_omit() {
+        let string = "{{ package_filters | default(value=omit()) }}";
+        let e = render_string(string, &Vars::new()).unwrap_err();
+        dbg!(&e);
+        assert_eq!(e.kind(), error::ErrorKind::OmitParam)
     }
 }
