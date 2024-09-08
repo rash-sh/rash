c79a3774302a6d4594d7cdef8ce193c42889b391
Alexander Gil <pando855@gmail.com>
2024-09-08 14:24:56 +0200

    refactor(core): Improbe `merge_json` performance
    Signed-off-by: Alexander Gil <pando855@gmail.com>

--- rash_core/src/utils.rs
+++ rash_core/src/utils.rs
@@ -13,24 +13,28 @@ pub fn parse_octal(s: &str) -> Result<u32> {
 }
 
 pub fn merge_json(a: &mut serde_json::Value, b: serde_json::Value) {
-    match (a, b) {
-        (a @ &mut serde_json::Value::Object(_), serde_json::Value::Object(b)) => {
-            let a = a.as_object_mut().unwrap();
-            for (k, v) in b {
-                if v.is_array() && a.contains_key(&k) && a.get(&k).as_ref().unwrap().is_array() {
-                    let mut _a = a.get(&k).unwrap().as_array().unwrap().to_owned();
-                    _a.append(&mut v.as_array().unwrap().to_owned());
-                    a[&k] = serde_json::Value::from(_a);
-                } else if v.is_u64() && a.contains_key(&k) && a.get(&k).as_ref().unwrap().is_u64() {
-                    let _a = a.get(&k).unwrap().as_u64().unwrap().to_owned();
-                    let _v = v.as_u64().unwrap().to_owned();
-                    a[&k] = serde_json::Value::from(_a + _v);
-                } else {
-                    merge_json(a.entry(k).or_insert(serde_json::Value::Null), v);
+    if let (Some(a_map), Some(b_map)) = (a.as_object_mut(), b.as_object()) {
+        for (k, v) in b_map {
+            match (a_map.get_mut(k), &v) {
+                (Some(serde_json::Value::Array(a_arr)), serde_json::Value::Array(b_arr)) => {
+                    a_arr.extend(b_arr.clone());
+                }
+                (Some(serde_json::Value::Number(a_num)), serde_json::Value::Number(b_num))
+                    if a_num.is_u64() && b_num.is_u64() =>
+                {
+                    let sum = a_num.as_u64().unwrap() + b_num.as_u64().unwrap();
+                    a_map.insert(k.to_string(), serde_json::Value::from(sum));
+                }
+                (Some(a_val), _) => {
+                    merge_json(a_val, v.clone());
+                }
+                (None, _) => {
+                    a_map.insert(k.to_string(), v.clone());
                 }
             }
         }
-        (a, b) => *a = b,
+    } else {
+        *a = b;
     }
 }
 
@@ -50,5 +54,128 @@ mod tests {
         assert_eq!(parse_octal("0444").unwrap(), 0o444);
         assert_eq!(parse_octal("600").unwrap(), 0o600);
         assert_eq!(parse_octal("0600").unwrap(), 0o600);
+    }
+
+    #[test]
+    fn test_merge() {
+        let mut a = json!({ "a": { "b": "foo" } });
+        let b = json!({ "a": { "c": "boo" } });
+        merge_json(&mut a, b);
+
+        assert_eq!(a.get("a").unwrap(), &json!({ "b": "foo", "c": "boo" }));
+    }
+
+    #[test]
+    fn test_merge_overlapping() {
+        let mut a = json!({ "a": "foo" });
+        let b = json!({ "a": "boo" });
+        merge_json(&mut a, b);
+        assert_eq!(a.get("a").unwrap(), &json!("boo"));
+    }
+
+    #[test]
+    fn test_merge_overlapping_nested() {
+        let mut a = json!({ "a": { "c": "boo" } });
+        let b = json!({ "a": { "b": "foo" } });
+        merge_json(&mut a, b);
+        assert_eq!(a.get("a").unwrap(), &json!({ "b": "foo", "c": "boo" }));
+    }
+
+    #[test]
+    fn test_merge_mixed_types() {
+        let mut a = json!({ "a": "simple_value" });
+        let b = json!({ "a": { "b": "foo" } });
+        merge_json(&mut a, b);
+        assert_eq!(a.get("a").unwrap(), &json!({ "b": "foo" }));
+    }
+
+    #[test]
+    fn test_merge_with_empty() {
+        let mut a = json!({});
+        let b = json!({ "a": "foo" });
+        merge_json(&mut a, b);
+        assert_eq!(a.get("a").unwrap(), &json!("foo"));
+
+        let mut a = json!({ "a": "foo" });
+        let b = json!({});
+        merge_json(&mut a, b);
+        assert_eq!(a.get("a").unwrap(), &json!("foo"));
+    }
+
+    #[test]
+    fn test_merge_deeply_nested() {
+        let mut a = json!({ "a": { "b": { "d": "boo" } } });
+        let b = json!({ "a": { "b": { "c": "foo" } } });
+        merge_json(&mut a, b);
+        assert_eq!(
+            a.get("a").unwrap(),
+            &json!({ "b": { "c": "foo", "d": "boo" } })
+        );
+    }
+
+    #[test]
+    fn test_merge_deeply_nested_partially_overlap() {
+        let mut a = json!({ "a": { "b": { "d": "boo", "e": "world" } } });
+        let b = json!({ "a": { "b": { "c": "foo", "e": "hello" } } });
+        merge_json(&mut a, b);
+        assert_eq!(
+            a.get("a").unwrap(),
+            &json!({ "b": { "c": "foo", "d": "boo", "e": "hello" } })
+        );
+    }
+
+    #[test]
+    fn test_merge_add_top_level() {
+        let mut a = json!({ "a": "foo" });
+        let b = json!({ "b": "boo" });
+        merge_json(&mut a, b);
+        assert_eq!(a.get("a").unwrap(), &json!("foo"));
+        assert_eq!(a.get("b").unwrap(), &json!("boo"));
+    }
+
+    #[test]
+    fn test_merge_both_empty() {
+        let mut a = json!({});
+        let b = json!({});
+        merge_json(&mut a, b);
+        assert_eq!(a, json!({}));
+    }
+
+    #[test]
+    fn test_merge_seq_concatenation() {
+        let mut a = json!({ "a": vec![1, 2, 3] });
+        let b = json!({ "a": vec![4, 5, 6] });
+        merge_json(&mut a, b);
+        assert_eq!(a.get("a").unwrap(), &json!(vec![1, 2, 3, 4, 5, 6]));
+    }
+
+    #[test]
+    fn test_merge_seq_with_non_seq() {
+        let mut a = json!({ "a": vec![1, 2, 3] });
+        let b = json!({ "a": "override" });
+        merge_json(&mut a, b);
+        assert_eq!(a.get("a").unwrap(), &json!("override"));
+    }
+
+    #[test]
+    fn test_merge_nested_seq_concatenation() {
+        let mut a = json!({ "a": { "b": vec![1, 2] } });
+        let b = json!({ "a": { "b": vec![3, 4] } });
+        merge_json(&mut a, b);
+        assert_eq!(
+            a.get("a").unwrap().get("b").unwrap(),
+            &json!(vec![1, 2, 3, 4])
+        );
+    }
+
+    #[test]
+    fn test_merge_deeply_nested_mixed_with_seq() {
+        let mut a = json!({ "a": { "b": { "c": vec![1, 2], "e": "hello" } } });
+        let b = json!({ "a": { "b": { "c": vec![3, 4], "e": "world" } } });
+        merge_json(&mut a, b);
+        assert_eq!(
+            a.get("a").unwrap(),
+            &json!({ "b": { "c": vec![1, 2, 3, 4], "e": "world" } })
+        );
     }
 }
