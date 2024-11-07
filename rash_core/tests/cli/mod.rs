3dd31ac9a646b84d6ec7d6af2d6a1acd71f466b0
Alexander Gil <pando855@gmail.com>
2024-11-07 17:51:22 +0100

    test(cli): Disable e2e tests for ARM
    Signed-off-by: Alexander Gil <pando855@gmail.com>

--- rash_core/tests/cli/mod.rs
+++ rash_core/tests/cli/mod.rs
@@ -4,6 +4,8 @@
 // And it `-S` support was introduced in coreutils 8.30:
 // https://lists.gnu.org/archive/html/info-gnu/2018-07/msg00001.html
 #[cfg(all(not(target_arch = "aarch64"), not(target_arch = "arm")))]
+mod args;
+#[cfg(all(not(target_arch = "aarch64"), not(target_arch = "arm")))]
 mod modules;
 
 use std::env;
@@ -35,32 +37,4 @@ pub fn execute_rash(args: &[&str]) -> (String, String) {
     dbg!(&stderr);
 
     (stdout, stderr)
-}
-
-#[test]
-fn test_script_arg() {
-    let script = r#"
-    - assert:
-        that:
-          - rash.path == "{{ rash.dir }}/rash"
-    "#;
-    let (stdout, _stderr) = execute_rash(&["-s", script]);
-    assert!(stdout.contains("ok"));
-}
-
-#[test]
-fn test_script_arg_and_script_file() {
-    let script = r#"
-    - assert:
-        that:
-          - rash.path == "{{ rash.dir }}/script.rh"
-    "#;
-    let (stdout, _stderr) = execute_rash(&["-s", script, "script.rh"]);
-    assert!(stdout.contains("ok"));
-}
-
-#[test]
-fn test_no_script_arg_and_no_script_file() {
-    let (_stdout, stderr) = execute_rash(&[]);
-    assert!(stderr.contains("Please provide either <SCRIPT_FILE> or --script."));
 }
