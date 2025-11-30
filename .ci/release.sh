4ee202d35edd24acf74c30d2b4bf9dda5250d303
Alexander Gil <pando855@gmail.com>
2025-12-01 00:59:38 +0100

    fix: Ensure CHANGELOG commit IDs are correct on release process

--- .ci/release.sh
+++ .ci/release.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if ! [ "$(git rev-list --count origin/master..HEAD)" -eq 0 ]; then
+    echo "There are commits in this branch. Please merge them first."
+    echo "CHANGELOG template needs master commit ID."
+    exit 1
+fi
+
 # bump version
 vim Cargo.toml
 make update-version
